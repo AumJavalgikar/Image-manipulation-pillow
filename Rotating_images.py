669fbdb991c972abec94ce8a2984449e095f4e09
aum <aumjavalgikar@gmail.com>
2022-04-12 16:55:11 +0530

    learned how to flip images :D

--- Rotating_images.py
+++ Rotating_images.py
@@ -4,3 +4,6 @@ imOb = Image.open('images/zophie.png')
 imOb.rotate(90).save('images/rotated_1.png')
 imOb.rotate(180).save('images/rotated_2.png')
 imOb.rotate(270).save('images/rotated_3.png')
+imOb.transpose(Image.FLIP_LEFT_RIGHT).save('images/flipped_left.png')
+imOb.transpose(Image.FLIP_TOP_BOTTOM).save('images/flipped_top.png')
+
